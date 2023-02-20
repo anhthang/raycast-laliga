13f8c0ce50cc2330db37aaf7c389826fd9b3d5e0
Anh Thang Bui <buianhthang89@gmail.com>
2023-02-20 17:24:18 +0700

    fix: other competition date time

--- src/components/matchday.tsx
+++ src/components/matchday.tsx
@@ -14,12 +14,12 @@ export default function Matchday(props: PropsType) {
   return (
     <List.Section key={name} title={name}>
       {matches.map((match) => {
-        let icon: Image.ImageLike;
+        let icon: Image.ImageLike = Icon.Clock;
         if (match.status.toLowerCase().includes("half")) {
           icon = { source: Icon.Livestream, tintColor: Color.Red };
         } else if (match.status === "FullTime") {
           icon = { source: Icon.CheckCircle, tintColor: Color.Green };
-        } else {
+        } else if (match.time) {
           icon = Icon.Calendar;
         }
 
@@ -33,13 +33,14 @@ export default function Matchday(props: PropsType) {
           },
         ];
 
+        const title = match.time
+          ? format(new Date(match.date), props.format || "eee dd.MM.yyyy HH:mm")
+          : "TBC";
+
         return (
           <List.Item
             key={match.id}
-            title={format(
-              new Date(match.date),
-              props.format || "eee dd.MM.yyyy"
-            )}
+            title={title}
             subtitle={
               match.status === "PreMatch"
                 ? `${match.home_team.nickname} - ${match.away_team.nickname}`
